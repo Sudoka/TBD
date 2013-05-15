d9fea1a1a28dde44e9ea3554da9cb96475ad6112
Koa Nies <koa.nies>
2013-05-14 18:43:38 -0700

    Baseline

---
+++ cs110/src/com/example/cs110/Tools.java
@@ -0,0 +1,37 @@
+package com.example.cs110;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+//test
+public class Tools extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tools);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tools, menu);
+		return true;
+	}
+	public void goBAC(View view){
+    	Intent intent=new Intent (this,BAC.class);
+    	startActivity(intent);
+    }
+
+    public void goPairings(View view){
+    	Intent intent=new Intent (this, Pairings.class);
+    	startActivity (intent);
+    }
+    
+    public void goLocations(View view){
+    	Intent intent=new Intent (this, Locations.class);
+    	startActivity (intent);
+    }
+}
