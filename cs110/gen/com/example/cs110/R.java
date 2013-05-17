a91ae0451ae690ae2ac1c7941df3d7b2ac7fabd8
Sanjana <Sanjana@128.54.167.216>
2013-05-16 19:18:23 -0700

    try this one !!

--- cs110/gen/com/example/cs110/R.java
+++ cs110/gen/com/example/cs110/R.java
@@ -7,8 +7,6 @@
 
 package com.example.cs110;
 
-import android.view.View;
-
 public final class R {
     public static final class attr {
     }
@@ -23,10 +21,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int glass=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int logo=0x7f020002;
-        public static final int wineca=0x7f020003;
+        public static final int bottles=0x7f020000;
+        public static final int glass=0x7f020001;
+        public static final int glasses=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int logo1=0x7f020005;
+        public static final int wineca=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f08001a;
@@ -61,34 +62,33 @@ public final class R {
         public static final int activity_bac=0x7f030000;
         public static final int activity_display_message=0x7f030001;
         public static final int activity_education=0x7f030002;
-        public static final int activity_locations=0x7f030003;
-        public static final int activity_main=0x7f030004;
-        public static final int activity_pairings=0x7f030005;
-        public static final int activity_settings=0x7f030006;
-        public static final int activity_tools=0x7f030007;
-		public static final int activity_types=0x7f030008;
-		public static final int activity_pricing=0x7f030009;
-		public static final int activity_history=0x7f03000a;
-		public static final int activity_etiquette=0x7f03000b;
-		public static final int activity_production=0x7f03000c;
-		public static final int activity_Regions=0x7f03000c;
+        public static final int activity_etiquette=0x7f030003;
+        public static final int activity_history=0x7f030004;
+        public static final int activity_locations=0x7f030005;
+        public static final int activity_main=0x7f030006;
+        public static final int activity_pairings=0x7f030007;
+        public static final int activity_pricing=0x7f030008;
+        public static final int activity_production=0x7f030009;
+        public static final int activity_regions=0x7f03000a;
+        public static final int activity_settings=0x7f03000b;
+        public static final int activity_tools=0x7f03000c;
+        public static final int activity_types=0x7f03000d;
     }
     public static final class menu {
         public static final int bac=0x7f070000;
         public static final int bacfinal=0x7f070001;
         public static final int display_message=0x7f070002;
         public static final int education=0x7f070003;
-        public static final int home=0x7f070004;
-        public static final int locations=0x7f070005;
-        public static final int main=0x7f070006;
-        public static final int pairings=0x7f070007;
-        public static final int settings=0x7f070008;
-        public static final int tools=0x7f070009;
-        public static final int types=0x7f070009;
+        public static final int history=0x7f070004;
+        public static final int home=0x7f070005;
+        public static final int locations=0x7f070006;
+        public static final int main=0x7f070007;
+        public static final int pairings=0x7f070008;
         public static final int pricing=0x7f070009;
-        public static final int history=0x7f070009;
-        public static final int production=0x7f070009;
-        public static final int Regions=0x7f070009;
+        public static final int regions=0x7f07000a;
+        public static final int settings=0x7f07000b;
+        public static final int tools=0x7f07000c;
+        public static final int types=0x7f07000d;
     }
     public static final class string {
         public static final int Education=0x7f05001f;
--- cs110/src/com/example/cs110/Regions.java
+++ cs110/src/com/example/cs110/Regions.java
@@ -13,13 +13,13 @@ public class Regions extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_Regions);
+		setContentView(R.layout.activity_regions);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.Regions, menu);
+		getMenuInflater().inflate(R.menu.regions, menu);
 		return true;
 	}
 }
