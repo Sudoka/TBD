73fa83d3439bd76685fc8ba7d1275b9ff76ba29d
Luke Pickett <lpickett@ucsd.edu>
2013-05-20 00:07:40 -0700

    Fixed BAC

--- cs110/src/com/example/cs110/BAC.java
+++ cs110/src/com/example/cs110/BAC.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class BAC extends Activity {
 	
@@ -35,33 +36,52 @@ public class BAC extends Activity {
     }
     
     	
-    public void calcBAC(){
+    public void calcBAC(View view){
     	double rate;
     	EditText drinks = (EditText) findViewById(R.id.editNumOfDrinks);
     	EditText weight = (EditText) findViewById(R.id.editWeight);
         EditText hours = (EditText) findViewById(R.id.editHours);
-        EditText result = (EditText) findViewById(R.id.result);
+        TextView result = (TextView) findViewById(R.id.result);
+        TextView warning= (TextView) findViewById(R.id.BACwarning);
         RadioButton r=(RadioButton) findViewById(R.id.male);
     	boolean male=r.isChecked();
-    	if (male)
-    	{
-    		rate=.73;
-    	}
-    	else
-    	{
-    		rate=.66;
-    	}
+
     	//calculates BAC
     	//TODO This doesn't calculate the BAC correctly. Figure out why
     	
-    	double fullBAC = (Double.parseDouble(drinks.getText().toString())*.13*rate*5.14/(Double.parseDouble(weight.getText().toString()))-(Double.parseDouble(hours.getText().toString())));
-    	result.setText(String.valueOf(fullBAC));
-    	if (fullBAC<0)
-    	{
-    		result.setText(String.valueOf(0));
-    	}
-    }
+    	int d=Integer.parseInt(drinks.getText().toString());
+    	int w=Integer.parseInt(weight.getText().toString());
+    	int h=Integer.parseInt(hours.getText().toString());
     	
+    	double fullBAC = calculateBAC(w,d,h,male);
+    	String BACstring=String.format("%.3g%n", fullBAC);
+    	result.setText(BACstring);
+    	//TODO fix the warning system
+    	/*if (fullBAC<.08)
+    	{
+    		warning.setText("DO NOT DRIVE");
+    		warning.setTextColor(0xFF0000);
+    		return;
+    	}
+    	else if (fullBAC>0)
+    	{
+    		warning.setText("Impaired");
+    		warning.setTextColor(0xFFFF00);
+    	}
+    	else
+    	{
+    		warning.setText("Sober");
+    		warning.setTextColor(0x00FF00);
+    	}*/
+
+    }
+    	private double calculateBAC(int weight, int drinks, int hours, boolean male)
+    	{
+    		double r;
+    		if (male) r=.68; else r=.55;
+    		double bac=(drinks*0.06*100*(1.055/weight)*r)-(0.015*hours);
+    		if (bac<0) return 0; else return bac;
+    	}
     	
     	
     	
