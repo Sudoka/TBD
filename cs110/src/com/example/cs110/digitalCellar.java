7cdf681ff2ccc50391c39e24e7400a25d1ea25ce
Steven Rogalsky <stevestar88@aol.com>
2013-05-19 22:25:56 -0700

    wine libraries stuff

---
+++ cs110/src/com/example/cs110/digitalCellar.java
@@ -0,0 +1,23 @@
+package com.example.cs110;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class digitalCellar extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_digital_cellar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
